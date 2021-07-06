0be4adb1dee48125c266cc93c36d0b9df1cdb4c0
junkiYoon <juneki4933@gmail.com>
2021-07-06 18:09:21 +0900

    ADD auth router
    add auth router

--- project/main.py
+++ project/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from project.apps import auth
+
 
 if __name__ == '__main__':
     app = FastAPI()
+
+    app.include_router(auth.router)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
