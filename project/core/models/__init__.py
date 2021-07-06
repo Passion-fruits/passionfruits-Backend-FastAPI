54f000f26c38f023f01079931855c5054e8b5068
junkiYoon <juneki4933@gmail.com>
2021-07-06 18:06:08 +0900

    CREATE sqlalchemy init
    create engine, Base, session

---
+++ project/core/models/__init__.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from project.config import MYSQL_DB_URL
+
+engine = create_engine(MYSQL_DB_URL, pool_recycle=3600, echo=True)
+
+Base = declarative_base()
+
+Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+session = Session()
