59d101353ae5c081da4cbd99fe4fdc6f6919cf94
junkiYoon <juneki4933@gmail.com>
2021-07-08 20:08:56 +0900

    FIX sqlalchemy init
    add encoding utf-8, pool size 20, max overflow 20, pool pre ping True at engine, change auto flush true at session

--- project/core/models/__init__.py
+++ project/core/models/__init__.py
@@ -4,9 +4,17 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from project.config import MYSQL_DB_URL
 
-engine = create_engine(MYSQL_DB_URL, pool_recycle=3600, echo=True)
+engine = create_engine(
+    MYSQL_DB_URL,
+    encoding="utf-8",
+    pool_recycle=3600,
+    pool_size=20,
+    max_overflow=20,
+    pool_pre_ping=True,
+    echo=True
+)
 
 Base = declarative_base()
 
-Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
+Session = scoped_session(sessionmaker(bind=engine, autocommit=False))
 session = Session()
