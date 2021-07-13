b0a35bfcc1ada64a9fdad708620ed8aede8c5ef7
junkiYoon <juneki4933@gmail.com>
2021-07-13 20:18:11 +0900

    FIX user_like_song table
    add every column primary key True

--- project/core/models/user_like_song.py
+++ project/core/models/user_like_song.py
@@ -7,8 +7,8 @@ from project.core.models import Base
 class User_like_song(Base):
     __tablename__ = "user_like_song"
 
-    user_id = Column(Integer, ForeignKey("user.id"))
-    song_id = Column(Integer, ForeignKey("song.id"))
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    song_id = Column(Integer, ForeignKey("song.id"), primary_key=True)
 
     user = relationship("User", backref=backref("user_like"))
     song = relationship("Song", backref=backref("song_liked"))
