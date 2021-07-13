696a5f366ab67b95d5afb6d1e5aa61d19529ed74
junkiYoon <juneki4933@gmail.com>
2021-07-14 08:38:42 +0900

    CREATE is_follow function
    create is_follow function, and add is_follow in like_it function

--- project/utils/like.py
+++ project/utils/like.py
@@ -7,11 +7,21 @@ from project.utils import get_user_id
 from project.utils.song import is_song
 
 
+def is_like(session: Session, user_id: int, song_id: int):
+    like = session.query(User_like_song).\
+        filter(User_like_song.user_id == user_id, User_like_song.song_id == song_id).scalar()
+
+    return True if like else False
+
+
 def like_it(session: Session, user_email: str, song_id: int):
     user_id = get_user_id(session=session, email=user_email)
 
     if not is_song(session=session, song_id=song_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="could not find song matching this id")
+
+    if is_like(session=session, user_id=user_id, song_id=song_id):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="this user already like this song")
 
     session.add(
         User_like_song(user_id=user_id, song_id=song_id)
