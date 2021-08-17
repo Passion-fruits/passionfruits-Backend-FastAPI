50d43449c9c1471f80c97cf54a03ab59e66933a6
junkiYoon <juneki4933@gmail.com>
2021-08-17 17:02:24 +0900

    CREATE middleware
    create middleware

--- project/main.py
+++ project/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
 
@@ -6,6 +7,14 @@ from project.apps import auth, follow, like
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(follow.router)
