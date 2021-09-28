a156b75ccd75a9d23f8a4051653e48cce917d55a
junkiYoon <juneki4933@gmail.com>
2021-09-28 13:21:33 +0900

    UPDATE like
    change names adding "song"

--- project/apps/like.py
+++ project/apps/like.py
@@ -5,30 +5,30 @@ from typing import Optional
 from project.core.models import session_scope
 
 from project.utils.auth import token_check
-from project.utils.like import like_it, unlike_it, is_like
+from project.utils.like import like_song, unlike_song, is_like_song
 
 
 router = APIRouter()
 
 
-@router.post("/like/{song_id}", status_code=status.HTTP_201_CREATED, tags=["like"])
-async def like(song_id: int, Authorization: Optional[str] = Header(...)):
+@router.post("/like/song/{song_id}", status_code=status.HTTP_201_CREATED, tags=["like"])
+async def user_like_song(song_id: int, Authorization: Optional[str] = Header(...)):
     with session_scope() as session:
         user_id = token_check(token=Authorization, type="access")
 
-        like_it(session=session, user_id=user_id, song_id=song_id)
+        like_song(session=session, user_id=user_id, song_id=song_id)
 
         return {
             "message": "success"
         }
 
 
-@router.delete("/like/{song_id}", status_code=status.HTTP_200_OK, tags=["like"])
-async def unlike(song_id: int, Authorization: Optional[str] = Header(...)):
+@router.delete("/like/song/{song_id}", status_code=status.HTTP_200_OK, tags=["like"])
+async def user_unlike_song(song_id: int, Authorization: Optional[str] = Header(...)):
     with session_scope() as session:
         user_id = token_check(token=Authorization, type="access")
 
-        unlike_it(session=session, user_id=user_id, song_id=song_id)
+        unlike_song(session=session, user_id=user_id, song_id=song_id)
 
         return {
             "message": "success"
@@ -36,10 +36,10 @@ async def unlike(song_id: int, Authorization: Optional[str] = Header(...)):
 
 
 @router.get("/like", status_code=status.HTTP_200_OK, tags=["like"])
-async def is_likes(song_id: int, Authorization: Optional[str] = Header(...)):
+async def is_like_song(song_id: int, Authorization: Optional[str] = Header(...)):
     with session_scope() as session:
         user_id = token_check(token=Authorization, type="access")
 
         return {
-            "is_like": is_like(session=session, user_id=user_id, song_id=song_id)
+            "is_like": is_like_song(session=session, user_id=user_id, song_id=song_id)
         }
--- project/utils/like.py
+++ project/utils/like.py
@@ -7,19 +7,19 @@ from project.core.models.user_like_song import User_like_song
 from project.utils.song import is_song
 
 
-def is_like(session: Session, user_id: int, song_id: int):
+def is_like_song(session: Session, user_id: int, song_id: int):
     like = session.query(User_like_song).\
         filter(User_like_song.user_id == user_id, User_like_song.song_id == song_id).scalar()
 
     return True if like else False
 
 
-def like_it(session: Session, user_id: int, song_id: int):
+def like_song(session: Session, user_id: int, song_id: int):
 
     if not is_song(session=session, song_id=song_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="could not find song matching this id")
 
-    if is_like(session=session, user_id=user_id, song_id=song_id):
+    if is_like_song(session=session, user_id=user_id, song_id=song_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="this user already like this song")
 
     session.add(
@@ -28,12 +28,12 @@ def like_it(session: Session, user_id: int, song_id: int):
     session.commit()
 
 
-def unlike_it(session: Session, user_id: int, song_id: int):
+def unlike_song(session: Session, user_id: int, song_id: int):
 
     if not is_song(session=session, song_id=song_id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="could not find song matching this id")
 
-    if not is_like(session=session, user_id=user_id, song_id=song_id):
+    if not is_like_song(session=session, user_id=user_id, song_id=song_id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="this user does not like this song")
 
     del_like = session.query(User_like_song).\
