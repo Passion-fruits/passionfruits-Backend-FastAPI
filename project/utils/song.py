01a30ed1bc951f7fafa039982d4e2521fb3b692e
junkiYoon <juneki4933@gmail.com>
2021-10-30 04:35:07 +0900

    CREATE functions for recommend
    create get_lits_by_ids func, get_songs_by_ids, and get_playlist_by_ids

--- project/utils/song.py
+++ project/utils/song.py
@@ -1,9 +1,109 @@
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 
 from project.core.models.song import Song
+from project.core.models.profile import Profile
+from project.core.models.user_like_song import User_like_song
+from project.core.models.user_comment_song import User_comment_song
+from project.core.models.song_genre import Song_genre
+from project.core.models.genre_type import Genre_type
+from project.core.models.mood import Mood
+from project.core.models.mood_type import Mood_type
 
 
 def is_song(session: Session, song_id: int):
     song = session.query(Song).filter(Song.id == song_id).scalar()
 
     return True if song else False
+
+
+def get_lits_by_ids(session: Session, song_ids: list):
+    songs = session.query(
+        Song.id,
+        Song.title,
+        Song.description,
+        Genre_type.name,
+        Song.created_at,
+        Song.short_url,
+        Song.cover_url,
+        Song.user_id,
+        Profile.name,
+        func.count(User_like_song.user_id),
+        func.count(User_comment_song.user_id)
+    ).join(Profile, Song.user_id == Profile.user_id).\
+        join(User_like_song, Song.id == User_like_song.song_id).\
+        join(User_comment_song, Song.id == User_comment_song.song_id).\
+        join(Song_genre, Song.id == Song_genre.song_id).\
+        join(Genre_type, Song_genre.genre_type_id == Genre_type.id).\
+        filter(Song.id.in_(song_ids)).\
+        group_by(
+        Song.id,
+        Song.title,
+        Song.description,
+        Genre_type.name,
+        Song.short_url,
+        Song.cover_url,
+        Song.user_id,
+        Profile.name
+    )
+
+    return songs
+
+
+def get_songs_by_ids(session: Session, song_ids: list):
+    songs = session.query(
+        Song.id,
+        Song.title,
+        Song.user_id,
+        Profile.name,
+        Song.song_url,
+        Song.cover_url,
+        func.count(User_like_song.user_id)
+    ).join(Profile, Song.user_id == Profile.user_id).\
+        join(User_like_song, Song.id == User_like_song.song_id).\
+        filter(Song.id.in_(song_ids)).\
+        group_by(
+        Song.id,
+        Song.title,
+        Song.user_id,
+        Profile.name,
+        Song.song_url,
+        Song.cover_url
+    )
+
+    return songs
+
+
+def get_playlist_by_ids(session: Session, song_ids: list):
+    songs = session.query(
+        Song.id,
+        Song.title,
+        Genre_type.name,
+        Mood_type.name,
+        Song.created_at,
+        Song.user_id,
+        Profile.name,
+        Song.song_url,
+        Song.cover_url,
+        func.count(User_like_song.user_id)
+    ).join(Profile, Song.user_id == Profile.user_id).\
+        join(User_like_song, Song.id == User_like_song.song_id). \
+        join(Song_genre, Song.id == Song_genre.song_id).\
+        join(Genre_type, Song_genre.genre_type_id == Genre_type.id).\
+        join(Mood, Song.id == Mood.song_id).\
+        join(Mood_type, Mood.mood_type_id == Mood_type.id).\
+        filter(Song.id.in_(song_ids)).\
+        group_by(
+        Song.id,
+        Song.title,
+        Genre_type.name,
+        Genre_type.name,
+        Mood_type.name,
+        Song.created_at,
+        Song.user_id,
+        Profile.name,
+        Song.song_url,
+        Song.cover_url
+    )
+
+    return songs
