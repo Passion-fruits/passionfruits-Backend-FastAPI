f293046060e69753caee26b9f5a764f3c2ba6cd8
junkiYoon <juneki4933@gmail.com>
2021-11-08 16:53:24 +0900

    CREATE is_user_like_playlist endpoint
    return boolean whether user like playlist or not

--- project/apps/like.py
+++ project/apps/like.py
@@ -5,7 +5,7 @@ from typing import Optional
 from project.core.models import session_scope
 
 from project.utils.auth import token_check
-from project.utils.like import like_song, unlike_song, is_like_song, like_playlist, unlike_playlist
+from project.utils.like import like_song, unlike_song, is_like_song, like_playlist, unlike_playlist, is_like_playlist
 
 
 router = APIRouter()
@@ -66,4 +66,14 @@ async def user_like_playlist(playlist_id: int, Authorization: Optional[str] = He
 
         return {
             "message": "success"
+        }
+
+
+@router.get("/like/playlist/{playlist_id}", status_code=status.HTTP_200_OK, tags=["like"])
+async def is_user_like_playlist(playlist_id: int, Authorization: Optional[str] = Header(...)):
+    with session_scope() as session:
+        user_id = token_check(token=Authorization, type="access")
+
+        return {
+            "is_like": is_like_playlist(session=session, user_id=user_id, playlist_id=playlist_id)
         }
