8cb91bf5179940c267774a8f3e9fb178fe4c60bf
junkiYoon <juneki4933@gmail.com>
2021-11-08 11:39:25 +0900

    UPDATE kdt table
    set default 0 str

--- project/core/models/kdt.py
+++ project/core/models/kdt.py
@@ -8,8 +8,8 @@ class Kdt(Base):
     __tablename__ = "kdt"
 
     user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
-    add_kdt = Column(BIGINT, nullable=False, server_default=0)
-    donate_kdt = Column(BIGINT, nullable=False, server_default=0)
-    reward_kdt = Column(BIGINT, nullable=False, server_default=0)
+    add_kdt = Column(BIGINT, nullable=False, server_default="0")
+    donate_kdt = Column(BIGINT, nullable=False, server_default="0")
+    reward_kdt = Column(BIGINT, nullable=False, server_default="0")
 
     user = relationship("User", backref=backref("kdt", order_by=user_id))
